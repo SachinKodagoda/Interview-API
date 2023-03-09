64a012d481c17d1155436e8b2d35ce2dfe7c1436
Duminda Kodagoda <duminda.g.k@gmail.com>
2023-03-10 03:27:57 +0530

    minor clean up

--- app/page.tsx
+++ app/page.tsx
@@ -9,35 +9,37 @@ import styles from "./page.module.css";
 export default function Home() {
   const [imageName, setImageName] = useState<"home" | "cart">("home");
   return (
-    <main className={styles.main}>
+    <>
       <Header />
-      <section className={styles.section_body}>
-        <section className={styles.description_section}>
-          <p className={styles.description}>
-            <Image
-              src="/images/bulb.svg"
-              alt=""
-              width="30"
-              height="30"
-              className={styles.desc_index}
+      <main className={styles.main}>
+        <section className={styles.section_body}>
+          <section className={styles.description_section}>
+            <p className={styles.description}>
+              <Image
+                src="/images/bulb.svg"
+                alt=""
+                width="30"
+                height="30"
+                className={styles.desc_index}
+              />
+              <span className={styles.desc}>
+                <b>Hello!</b> Thank you for your interest. This is the{" "}
+                <b>second stage</b> of the interview process in which we want to
+                get a sense of what you&apos;re technically capable of.
+              </span>
+            </p>
+            <Task imageName={imageName} setImageName={setImageName} />
+            <Marking />
+          </section>
+          <picture className={styles.picture_tag}>
+            <img
+              src={`/images/${imageName}.jpg`}
+              alt="home page"
+              className={styles.home_page_img}
             />
-            <span className={styles.desc}>
-              <b>Hello!</b> Thank you for your interest. This is the{" "}
-              <b>second stage</b> of the interview process in which we want to
-              get a sense of what you&apos;re technically capable of.
-            </span>
-          </p>
-          <Task imageName={imageName} setImageName={setImageName} />
-          <Marking />
+          </picture>
         </section>
-        <picture className={styles.picture_tag}>
-          <img
-            src={`/images/${imageName}.jpg`}
-            alt="home page"
-            className={styles.home_page_img}
-          />
-        </picture>
-      </section>
-    </main>
+      </main>
+    </>
   );
 }
