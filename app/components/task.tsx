c91f1626bc92795b8d93a2884e9192b71b62f210
Duminda Kodagoda <duminda.g.k@gmail.com>
2023-03-10 04:06:23 +0530

    finalized

--- app/components/task.tsx
+++ app/components/task.tsx
@@ -17,13 +17,9 @@ export default function Task({ imageName, setImageName }: TProps) {
       <div className={styles.topic_desc}>
         <div className={styles.item_nr}>1.</div>
         <div className={styles.desc_item}>
-          Create a simple <b>responsive e-commerce</b> app using <b>NextJs</b>.
-        </div>
-        <div className={styles.item_nr}>2.</div>
-        <div className={styles.desc_item}>
-          It should include a <b>Home page</b> and <b>Add-To-Cart Modal</b>.
-          Click the button to switch between the home page and the Add-To-Cart
-          Modal{" "}
+          You have to create a <b>responsive e-commerce</b> app using{" "}
+          <b>NextJs</b>. It should include a <b>Home page</b> and{" "}
+          <b>Add to Cart Modal</b>. Click here (
           <button
             type="button"
             className={styles.change_view}
@@ -31,15 +27,23 @@ export default function Task({ imageName, setImageName }: TProps) {
               setImageName((a) => (a === "home" ? "cart" : "home"));
             }}
           >
-            {imageName === "home" ? "Cart Modal" : "Home Page"}
+            {imageName === "home" ? "Cart" : "Home"}
           </button>
+          ) to switch between the home page and the Add-To-Cart Modal{" "}
         </div>
-        <div className={styles.item_nr}>3.</div>
+        <div className={styles.item_nr}>2.</div>
         <div className={styles.desc_item}>
           In terms of functionality, A user should be able to add <b>one</b> or{" "}
           <b>more</b> items into his cart by clicking the{" "}
-          <b>&quot;Add to Cart&quot;</b> button on the product box, and the
-          number on the <b>cart icon</b> (
+          <b>&quot;Add to Cart&quot;</b> button ({" "}
+          <Image
+            src="/images/add_to_cart.png"
+            alt=""
+            width="120"
+            height="30"
+            className={styles.cart_item}
+          />
+          ) on the product box, and the number on the <b>cart icon</b> (
           <Image
             src="/images/cart_icon.png"
             alt=""
@@ -49,7 +53,7 @@ export default function Task({ imageName, setImageName }: TProps) {
           />
           ) should change accordingly.
         </div>
-        <div className={styles.item_nr}>4.</div>
+        <div className={styles.item_nr}>3.</div>
         <div className={styles.desc_item}>
           The backend should be simple and built with <b>NodeJS</b>, and it
           could be hosted elsewhere or included in the same app{" "}
@@ -66,28 +70,28 @@ export default function Task({ imageName, setImageName }: TProps) {
           </Link>
           .
         </div>
-        <div className={styles.item_nr}>5.</div>
+        <div className={styles.item_nr}>4.</div>
         <div className={styles.desc_item}>
           Include a <b>ReadMe</b> file with instructions on how to view your
           project, how it was made, and any other information.{" "}
           <b>(Keep in mind that documentation is your friend)</b>
         </div>
-        <div className={styles.item_nr}>6.</div>
+        <div className={styles.item_nr}>5.</div>
         <div className={styles.desc_item}>
           Do a proper breakdown of components and improve the user interface so
           that it looks professional and best represents your knowledge.
         </div>
-        <div className={styles.item_nr}>7.</div>
+        <div className={styles.item_nr}>6.</div>
         <div className={styles.desc_item}>
           Ignore the functionality of links (name, tags, wishlist) but style
           them appropriately.
         </div>
-        <div className={styles.item_nr}>8.</div>
+        <div className={styles.item_nr}>7.</div>
         <div className={styles.desc_item}>
           We are giving you <b>one week</b> to complete this, but if you need
           more time, please let us know via email.
         </div>
-        <div className={styles.item_nr}>9.</div>
+        <div className={styles.item_nr}>8.</div>
         <div className={styles.desc_item}>
           <b>Image Resources</b> can be found{" "}
           <Link href="/" className={styles.cart_item}>
